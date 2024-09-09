8a88cc65a034e15fe499ec31d687a937a6b8f80b
Leandro Ordonez <leandro.ordonez.ante@gmail.com>
2024-09-10 00:16:51 +0200

    Update geocoding routes to remove authentication

--- backend/src/routes/geocodingRoutes.js
+++ backend/src/routes/geocodingRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const geocodingController = require('../controllers/geocodingController');
 const { authenticate } = require('../middleware/auth');
 
-router.get('/forward', authenticate, geocodingController.forwardGeocode);
-router.get('/reverse', authenticate, geocodingController.reverseGeocode);
+router.get('/forward', geocodingController.forwardGeocode);
+router.get('/reverse', geocodingController.reverseGeocode);
 router.get('/driving-distance', authenticate, geocodingController.calculateDrivingDistance);
 
 module.exports = router;
